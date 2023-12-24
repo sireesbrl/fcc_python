fe066a76fb51b255cd556a5d6c21f1cd6e1c1a25
Shirish Baral <59017772+sireesbrl@users.noreply.github.com>
2023-12-24 13:26:30 +0545

    Add files via upload

---
+++ caesar_cipher.py
@@ -0,0 +1,18 @@
+text = input("Enter a text: ").strip()
+shift = int(input("Enter offset: ").strip())
+
+def caesar(message, offset):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    encrypted_text = ''
+
+    for char in message.lower():
+        if char == ' ':
+            encrypted_text += char
+        else:
+            index = alphabet.find(char)
+            new_index = (index + offset) % len(alphabet)
+            encrypted_text += alphabet[new_index]
+
+    print('encrypted text:', encrypted_text)
+
+caesar(text, shift)
